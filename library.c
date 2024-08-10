c14c5b9a96547a68bc74a4a3ffa3a33b46b2986b
Adam <elzadam11@gmail.com>
2024-08-10 20:36:50 +0100

    Added code, modified gitignore

--- library.c
+++ library.c
@@ -1,7 +1,94 @@
 #include "library.h"
-
 #include <stdio.h>
+#include <process.h>
 
-void hello(void) {
-    printf("Hello, World!\n");
+// ------------------------------------------------
+// Some Basic Info
+// ------------------------------------------------
+
+#ifndef PI
+#define PI 3.14159265358979323846f
+#endif
+#ifndef DEG2RAD
+#define DEG2RAD (PI/180.0f)
+#endif
+#ifndef RAD2DEG
+#define RAD2DEG (180.0f/PI)
+#endif
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+// -------------------------------------------------
+// Color
+// -------------------------------------------------
+
+void DisplayRedColor(char *txt) {
+    printf("%sred\n", KRED);
+}
+
+void DisplayGreenColor(char *txt) {
+    printf("%sgreen\n", KGRN);
+}
+
+void DisplayYellowColor(char *txt) {
+    printf("%syellow\n", KYEL);
+}
+
+void DisplayBlueColor(char *txt) {
+    printf("%sblue\n", KBLU);
+}
+
+void DisplayMagentaColor(char *txt) {
+    printf("%smagenta\n", KMAG);
+}
+
+void DisplayCyanColor(char *txt) {
+    printf("%scyan\n", KCYN);
+}
+
+void DisplayWhiteColor(char *txt) {
+    printf("%swhite\n", KWHT);
+}
+
+void DisplayNormalColor(char *txt) {
+    printf("%snormal\n", KNRM);
+}
+
+// ------------------------------------------------
+// Window
+// ------------------------------------------------
+
+void CloseWindow() {
+    exit(0);
+}
+
+// ------------------------------------------------
+// Math
+// ------------------------------------------------
+
+float sqrt(float x) {
+    printf("sqrt(%f) = %f\n", x, sqrt(x));
+}
+
+float sin(float x) {
+    printf("sin(%f) = %f\n", x, sin(x));
+}
+
+float cos(float x) {
+    printf("cos(%f) = %f\n", x, cos(x));
+}
+
+float tan(float x) {
+    printf("tan(%f) = %f\n", x, tan(x));
+}
+
+float pow(float x, float y) {
+    printf("pow(%f, %f) = %f\n", x, y, pow(x, y));
 }
--- library.h
+++ library.h
@@ -1,6 +1,33 @@
 #ifndef AIRLIB_LIBRARY_H
 #define AIRLIB_LIBRARY_H
 
-void hello(void);
+// -------------------------------------------------
+// Color
+// -------------------------------------------------
+
+void DisplayRedColor(char *txt);
+void DisplayGreenColor(char *txt);
+void DisplayYellowColor(char *txt);
+void DisplayBlueColor(char *txt);
+void DisplayMagentaColor(char *txt);
+void DisplayCyanColor(char *txt);
+void DisplayWhiteColor(char *txt);
+void DisplayNormalColor(char *txt);
+
+// ------------------------------------------------
+// Window
+// ------------------------------------------------
+
+void CloseWindow();
+
+// ------------------------------------------------
+// Math
+// ------------------------------------------------
+
+float sqrt(float x);
+float sin(float x);
+float cos(float x);
+float tan(float x);
+float pow(float x, float y);
 
 #endif //AIRLIB_LIBRARY_H
