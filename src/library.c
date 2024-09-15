da43d8bdad26cf0a164544e9f64123c07de45481
Adam Ellouze <elzadam11@gmail.com>
2024-09-15 17:58:00 +0100

    Modified library.c and .h to use camelCase instead of pascalCase

--- src/library.c
+++ src/library.c
@@ -4,35 +4,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-
-// ------------------------------------
-// ANSI Escape Colors
-// ------------------------------------
-
-#define KCLS  "\e[2J" 
-#define KNRM  "\e[0m"
-#define KRED  "\e[31m"
-#define KGRN  "\e[32m"
-#define KYEL  "\e[33m" 
-#define KBLU  "\e[34m"
-#define KMAG  "\e[35m"
-#define KCYN  "\e[36m"
-#define KWHT  "\e[37m"
+#include "library.h"
 
 // ------------------------------------
 // Error handling
 // ------------------------------------
 
-/* If you ever have to use this,  */
-
-char *Airlib_Logger(char *text, unsigned int error, unsigned int warn, unsigned int info) {
+char *errorLog(char *text, unsigned int error, unsigned int warn, unsigned int info) {
   
   // Color
   if (error) printf(KRED);
   if (warn) printf(KYEL);
   if (info) printf(KBLU);
   
-  // text
+  // Text
   if (error) printf("error: ");
   if (warn) printf("warn: ");
   if (info) printf("info: ");
@@ -42,50 +27,10 @@ char *Airlib_Logger(char *text, unsigned int error, unsigned int warn, unsigned 
 }
 
 // ------------------------------------
-// Get Platform
-// ------------------------------------
-
-const char *Airlib_GetPlatform(void) {
-  #if defined(_WIN32)
-    return "Windows";
-  #elif defined(__APPLE__)
-    return "Apple";
-  #elif defined(__MACH__)
-    return "Apple";
-  #elif defined(TARGET_OS_EMBEDDED)
-    return "iOS Embedded";
-  #elif defined(TARGET_IPHONE_SIMULATOR)
-    return "iOS Emulator";
-  #elif defined(TARGET_OS_IPHONE)
-    return "iOS";
-  #elif defined(TARGET_OS_MAC)
-    return "MacOS";
-  #elif defined(__unix__)
-    return "Unix";
-  #elif defined(__linux__)
-    return "Linux";
-  #elif defined(__sun)
-    return "Solaris";
-  #elif defined(__hpux)
-    return "HP UX"; 
-  #elif defined(__DragonFly__)
-    return "DragonFly BSD";
-  #elif defined(__FreeBSD__)
-    return "FreeBSD";
-  #elif defined(__NetBSD__)
-    return "NetBSD";
-  #elif defined(__OpenBSD__)
-    return "OpenBSD";
-  #else 
-    #error "[ platform ] : unknown platform !"
-  #endif
-}
-
-// ------------------------------------
 // Color and Strings
 // ------------------------------------
 
-void DisplayText(char *text, char *color, unsigned int bold, unsigned int italic, unsigned int underline) {
+void richText(char *text, char *color, unsigned int bold, unsigned int italic, unsigned int underline) {
     printf("%s", color);
     if (bold) printf("\033[1m");
     if (italic) printf("\033[3m");
@@ -99,7 +44,7 @@ void DisplayText(char *text, char *color, unsigned int bold, unsigned int italic
 // Window
 // ------------------------------------
 
-void SegFault() {
+void segFault() {
     int *p = NULL;
     *p = 1;
 }
@@ -112,14 +57,14 @@ void clrscr() {
 // Clock and Time
 // ------------------------------------
 
-void SysSleep(int ms) {
+void sysSleep(int ms) {
     clock_t goal = ms * CLOCKS_PER_SEC / 1000;
     clock_t start = clock();
     while (clock() < goal) {
     }
 }
 
-float GetGameTime() {
+float getGameTime() {
     time_t rawtime;
     struct tm *timeinfo;
     time(&rawtime);
@@ -127,7 +72,7 @@ float GetGameTime() {
     printf("The time is %d:%d:%d\n", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
 }
 
-float StopWatch() {
+float stopWatch() {
     clock_t start = clock();
     clock_t end = clock();
     float seconds = (float) (end - start) / CLOCKS_PER_SEC;
@@ -138,7 +83,7 @@ float StopWatch() {
 // Drawing
 // ------------------------------------
 
-void DrawBox(int x, int y, int width, int height, char *color) {
+void drawBox(int x, int y, int width, int height, char *color) {
     printf("%s", color);
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
@@ -148,3 +93,4 @@ void DrawBox(int x, int y, int width, int height, char *color) {
     }
     printf(KNRM);  // Reset to normal
 }
+
--- src/library.h
+++ src/library.h
@@ -21,39 +21,70 @@
 // Error Handling
 // -------------------------------------------------
 
-char *Airlib_Logger(char *text, unsigned int error, unsigned int warn, unsigned int info);
+char *errorLog(char *text, unsigned int error, unsigned int warn, unsigned int info);
 
 // -------------------------------------------------
 // Get Platform
 // -------------------------------------------------
 
-const char *Airlib_GetPlatform(void);
+#if defined(_WIN32)
+    #define GET_PLATFORM "Windows"
+#elif defined(__APPLE__) || defined(__MACH__)
+    #define GET_PLATFORM "Apple"
+#elif defined(TARGET_OS_EMBEDDED)
+    #define GET_PLATFORM "iOS Embedded"
+#elif defined(TARGET_IPHONE_SIMULATOR)
+    #define GET_PLATFORM "iOS Emulator"
+#elif defined(TARGET_OS_IPHONE)
+    #define GET_PLATFORM "iOS"
+#elif defined(TARGET_OS_MAC)
+    #define GET_PLATFORM "MacOS"
+#elif defined(__unix__)
+    #define GET_PLATFORM "Unix"
+#elif defined(__linux__)
+    #define GET_PLATFORM "Linux"
+#elif defined(__sun)
+    #define GET_PLATFORM "Solaris"
+#elif defined(__hpux)
+    #define GET_PLATFORM "HP UX"
+#elif defined(__DragonFly__)
+    #define GET_PLATFORM "DragonFly BSD"
+#elif defined(__FreeBSD__)
+    #define GET_PLATFORM "FreeBSD"
+#elif defined(__NetBSD__)
+    #define GET_PLATFORM "NetBSD"
+#elif defined(__OpenBSD__)
+    #define GET_PLATFORM "OpenBSD"
+#else
+    #define GET_PLATFORM "Unknown platform"
+#endif
 
 // -------------------------------------------------
 // Color and Strings
 // -------------------------------------------------
 
-void DisplayText(char *text, char *color, unsigned int bold, unsigned int italic, unsigned int underline);
+void richText(char *text, char *color, unsigned int bold, unsigned int italic, unsigned int underline);
 
 // ------------------------------------------------
 // Window
 // ------------------------------------------------
 
-
-void SegFault();
+void segFault();
 void clrscr();
 
 // ------------------------------------------------
 // Clock and Time
 // ------------------------------------------------
 
-void SysSleep(int ms);
-float GetGameTime();
+void sysSleep(int ms);
+float getGameTime();
+float stopWatch();
 
 // ------------------------------------------------
 // Drawing
 // ------------------------------------------------
 
-void DrawBox(int x, int y, int width, int height, char *color);
+void drawBox(int x, int y, int width, int height, char *color);
 
 #endif //AIRLIB_LIBRARY_H
+
