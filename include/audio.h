3e6cb4f5bc2eca911b0b20c9f2050b475f1cd0a3
Adam Ellouze <elzadam11@gmail.com>
2024-11-06 16:46:01 +0100

    Rewrote audio.c to use PulseAudio

--- include/audio.h
+++ include/audio.h
@@ -1,7 +1,16 @@
 #ifndef AUDIO_H
 #define AUDIO_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Function declarations
 void PlaySoundFile(const char *filename);
 void StopSound();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // AUDIO_H
