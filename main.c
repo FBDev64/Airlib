c6e6b688294121f29c29def2860b55a4ea601f14
Adam <elzadam11@gmail.com>
2024-11-18 14:08:02 +0100

    placeholder test

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "include/old.h"
 
 void main() {
-    char che;
+    char che[100];
     // Example usage of placeholder function with KHOL
     placeholder("Enter your name here...", che);
     richText("Hello World", KBLU, 1, 0, 0);
