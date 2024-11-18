97d658f5232b52f60ee09d94a7bfdca6d22d6e8e
Adam <elzadam11@gmail.com>
2024-11-18 14:20:38 +0100

    placeholder test

--- main.c
+++ main.c
@@ -7,5 +7,5 @@ void main() {
     // Example usage of placeholder function with KHOL
     placeholder("Enter your name here...", che);
     richText("Hello World", KBLU, 1, 0, 0);
-    putchar(che);
+    printf("%s", che);
 }
