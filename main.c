7349b293a09f7dcfb552c4069e51f8f8d835d3e3
Adam <elzadam11@tutamail.com>
2024-12-11 13:58:33 +0100

    Attempt at GLX

--- main.c
+++ main.c
@@ -5,7 +5,6 @@ int main() {
     Win* window = createWindowInstance();
     window->create(600, 700, "WGL Test");
 
-    GLuint textureId = window->loadTexture("C:/Users/Adam/Pictures/ness.png");
     unsigned char textColor[3] = {34, 84, 2};
 
     while (!window->shouldClose()) {
@@ -17,8 +16,6 @@ int main() {
 
         // Reset color to white before drawing texture
         glColor3f(1.0f, 1.0f, 1.0f);  // White color
-
-        window->drawTexture(textureId, 100, 200, 200, 100);
 
         if (window->isButtonClicked(250, 300, 100, 40)) {
             window->drawText(100, 100, "Button clicked!", textColor);
